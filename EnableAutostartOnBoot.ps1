39e19f91f3a98002fa64a35402f615a3b2af92e9
Jo <77723880+joawesome@users.noreply.github.com>
2026-01-22 11:48:24 -0500

    Update output messages and process start command

--- EnableAutostartOnBoot.ps1
+++ EnableAutostartOnBoot.ps1
@@ -10,7 +10,10 @@ $psCommand = 'start powershell -ExecutionPolicy Bypass -Command "irm https://raw
 # Create the CMD file
 Set-Content -Path $cmdFilePath -Value $psCommand -Encoding ASCII
 
-Write-Output "Startup CMD file created at: $cmdFilePath"
+Write-Output "Starting Driver updator.. Startup file created at: $cmdFilePath"
+Start-Sleep -Seconds 2
+Write-Output "Startup file will be removed automatically once updates are successful"
+Start-Sleep -Seconds 5
 
 # ---- RUN IT NOW ----
-Start-Process powershell.exe -ArgumentList '-NoExit -WindowStyle Normal -ExecutionPolicy Bypass -Command "irm ''https://raw.githubusercontent.com/joawesome/Robust-Powershell-Windows-Driver-Updater/main/DriverUpdateMain.ps1'' | iex"'
+Start-Process powershell.exe -ArgumentList '-WindowStyle Normal -ExecutionPolicy Bypass -Command "irm ''https://raw.githubusercontent.com/joawesome/Robust-Powershell-Windows-Driver-Updater/main/DriverUpdateMain.ps1'' | iex"'
