01dee0cf274b6e33925d2fa7e0c5ef5c74ae73ec
Jo <77723880+joawesome@users.noreply.github.com>
2026-01-22 11:20:30 -0500

    Update command to start PowerShell in autostart script

--- EnableAutostartOnBoot.ps1
+++ EnableAutostartOnBoot.ps1
@@ -5,7 +5,7 @@ $startupPath = [Environment]::GetFolderPath("Startup")
 $cmdFilePath = Join-Path $startupPath "DriverUpdaterStartup.cmd"
 
 # Command to run
-$psCommand = 'powershell -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/joawesome/Robust-Powershell-Windows-Driver-Updater/main/DriverUpdateMain.ps1 | iex"'
+$psCommand = 'start powershell -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/joawesome/Robust-Powershell-Windows-Driver-Updater/main/DriverUpdateMain.ps1 | iex"'
 
 # Create the CMD file
 Set-Content -Path $cmdFilePath -Value $psCommand -Encoding ASCII
