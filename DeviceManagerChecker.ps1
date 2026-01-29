f25a5fba0af68eef79cf68aa6f6627cbe3247df2
Jo <77723880+joawesome@users.noreply.github.com>
2026-01-29 08:32:22 -0500

    Change device health scan to unknown device scan

--- DeviceManagerChecker.ps1
+++ DeviceManagerChecker.ps1
@@ -1,39 +1,9 @@
-Write-Host "Running device health scan..."
+Write-Host "Running unknown device scan..."
 
-$devices = Get-PnpDevice | Where-Object {
-    $_.Status -ne "OK" -or          # Not working properly
-    $_.Problem -ne 0 -or            # Any Device Manager error code
-    $_.FriendlyName -eq "Unknown device"
-}
+$devices = Get-PnpDevice | Where-Object Problem -eq 28
 
 if (-not $devices) {
-    Write-Host "No problematic devices detected."
-}
-else {
-    Write-Host "`nDevices requiring attention:`n"
-
-    $devices |
-        Sort-Object Problem, Class, FriendlyName |
-        Select-Object `
-            FriendlyName,
-            Class,
-            Status,
-            Problem,
-            @{Name="Meaning";Expression={
-                switch ($_.Problem) {
-                    0  { "OK" }
-                    10 { "Device cannot start" }
-                    14 { "Reboot required" }
-                    18 { "Driver must be reinstalled" }
-                    19 { "Registry issue" }
-                    21 { "Device disabled" }
-                    22 { "Disabled by user" }
-                    24 { "Device not present" }
-                    28 { "No driver installed" }
-                    29 { "Disabled by firmware" }
-                    31 { "Driver failed to load" }
-                    default { "Other problem" }
-                }
-            }} |
-        Format-Table -AutoSize
+    Write-Host "No unknown devices detected."
+} else {
+    $devices | Format-Table FriendlyName, Status, InstanceId -AutoSize
 }
